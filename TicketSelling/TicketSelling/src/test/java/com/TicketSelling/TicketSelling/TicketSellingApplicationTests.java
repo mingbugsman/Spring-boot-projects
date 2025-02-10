f3a2fc7deb51793e04724fcdd8dad54739281305
Tran Tuan Minh <ming18380@gmail.com>
2025-02-10 21:27:41 +0700

    Take second step for ticket selling

---
+++ TicketSelling/TicketSelling/src/test/java/com/TicketSelling/TicketSelling/TicketSellingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TicketSelling.TicketSelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
